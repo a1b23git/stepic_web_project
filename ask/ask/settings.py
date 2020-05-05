098664538ae19ae44562b0fd4892d59a76b4d8e7
a1b23git <63221057+a1b23git@users.noreply.github.com>
2020-05-05 22:36:38 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zlpg^h$yteudu-1s$v&82tf^1g@t2vnwmmp9m^@6$df_#^c0vi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
