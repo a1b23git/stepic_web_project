a971bb746a38c90c4b488f3c7a993c2e37fdda66
a1b23git <63221057+a1b23git@users.noreply.github.com>
2020-05-05 22:21:58 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
-from orders.views import test
+#from orders.views import test
+from . import views
 
 urlpatterns = [
-    path('/', test),
-    path('/login/', test),
-    path('/signup/', test),
-    path('/question/<123>/', test),
-    path('/ask/', test),
-    path('/popular/', test),
-    path('/new/', test)
+    url(r'.*', views.test),
     ]
