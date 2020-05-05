624ea96c5472a8ecb8fe1e9be7329684c1f1341b
a1b23git <63221057+a1b23git@users.noreply.github.com>
2020-05-05 21:54:38 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+#sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
 sudo /etc/init.d/gunicorn restart
 sudo gunicorn -b 0.0.0.0:8080 hello:app &
