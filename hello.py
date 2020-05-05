7fe50e7a2f79a190537dede22e574a273cb11e63
a1b23git <63221057+a1b23git@users.noreply.github.com>
2020-05-05 21:06:40 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-                  encoding="utf8")]
+    return "Hello!"
+    #return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+    #              encoding="utf8")]
