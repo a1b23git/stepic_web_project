7345b868af2a288c69c4e9ab2b79312e5767b973
a1b23git <63221057+a1b23git@users.noreply.github.com>
2020-05-06 16:23:54 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -80,11 +80,14 @@ DATABASES = {
    #     'ENGINE': 'django.db.backends.sqlite3',
    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
    # }
-   'ENGINE': 'django.db.backends.mysql', 
+   'default': {
+        'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'stepic_web',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': 'localhost',  
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        #'PORT': '3306',
+    }  
 }
 
 
