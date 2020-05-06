e8ed12f3d1169386ffe47d55ba2c8280944ce9fc
a1b23git <63221057+a1b23git@users.noreply.github.com>
2020-05-06 15:50:17 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -15,10 +15,10 @@ class Question(models.Model):
 
     title = models.CharField(default="", max_length=1024)
     text = models.TextField(default="")
-    added_at = models.DateField(null=True)
+    added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    likes = models.ManyToManyField(User, related_name="q_to_likes")
+    likes = models.ManyToManyField(User, related_name='question_like_user')
 
     def __str__(self):
         return self.title
@@ -29,7 +29,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField(default="")
-    added_at = models.DateField(null=True)
+    added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
