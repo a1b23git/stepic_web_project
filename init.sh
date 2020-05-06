40664c6ced7971d40fd5a854f6672d63c5abab22
a1b23git <63221057+a1b23git@users.noreply.github.com>
2020-05-06 20:57:44 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,4 +8,6 @@ sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/gunicorn.conf
 gunicorn -c /etc/gunicorn.d/gunicorn.conf.py ask.wsgi:application
 sudo /etc/init.d/gunicorn restart
 sudo gunicorn -b 0.0.0.0:8080 hello:app &
+cd ask/
+gunicorn --bind 0.0.0.0:8000 ask.wsgi &
 
